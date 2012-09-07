96012b213676052404a1dd0dcc193b7a89a2aa68
Greg Fyans <greg@blackred.co.uk>
2012-09-07 16:47:39 +0100

    jsLint'd

--- content/scripts/jquery.fullscreenbackground.js
+++ content/scripts/jquery.fullscreenbackground.js
@@ -1,4 +1,6 @@
 ﻿/// <reference path="_references.js" />
+/*jslint browser: true*/
+/*global $, jQuery, window, document, setInterval*/
 (function ($) {
     "use strict";
     $.fn.fullscreenbackground = function (opt) {
@@ -27,14 +29,98 @@
             options = {
                 files: opt.files,
                 interval: opt.interval || 5000,
-                transitionTime: opt.transitionTime || 600
+                transitionTime: opt.transitionTime || 600,
+                useScanlines: opt.useScanlines || true,
+                scanlineWidth: opt.scalineWidth || 2,
+                scanlineSpacing: opt.scanlineSpacing || 2,
+                scanlineRGBA: opt.scanlineRGA || [0, 0, 0, 0.5]
             },
             nextPosition = 0, // Hold our current position when iterating options.files
             previousPosition, // Hold the previous position when iterating options.files
             firstRun = true,
             newImage$,
 
-            init = function () {
+            resizeBackground = function () {
+                var imageWidth = newImage$.width(),
+                    imageHeight = newImage$.height(),
+                    containerWidth = fsbgContainer$.width(),
+                    containerHeight = fsbgContainer$.height(),
+                    imageRatio = imageWidth / imageHeight,
+                    containerRatio = containerWidth / containerHeight;
+
+                if (imageRatio <= containerRatio) {
+                    newImage$.css({ width: containerWidth + "px", height: "auto" });
+
+                    // offset the top to make it appear centered
+                    if (newImage$.height() > containerHeight) {
+                        newImage$.css({ top: "-" + ((imageHeight - containerHeight) / 2) + "px" });
+                    }
+                } else {
+                    newImage$.css({ height: containerHeight + "px", width: "auto" });
+
+                    // offset the left to make it appear centered
+                    if (newImage$.width() > containerWidth) {
+                        newImage$.css({ left: "-" + ((imageWidth - containerWidth) / 2) + "px" });
+                    }
+                }
+            },
+
+            queueImage = function () {
+                // Get the nextPosition image
+                var file = options.files[nextPosition],
+                    imageIdPrefix = "fsbg_image_",
+                    newImgContainerId = (imageIdPrefix + nextPosition);
+
+                newImage$ = $("<img />");
+                newImage$.attr("src", file);
+                newImage$.attr("id", newImgContainerId);
+                newImage$.css({
+                    "display": "none",
+                    "position": "absolute",
+                    "top": "0px",
+                    "left": "0px"
+                });
+
+                fsbgContainer$.find("img").addClass("delete-me");
+
+                fsbgContainer$.append(newImage$);
+
+                // We only want to transition in the new image if it's been fully loaded
+                newImage$.one("load", function () {
+                    resizeBackground();
+
+                    // Transition in the new one
+                    newImage$.fadeIn(options.transitionTime, function () {
+                        if (firstRun === true) {
+                            firstRun = false;
+                        } else {
+                            fsbgContainer$.find(".delete-me").remove();
+                        }
+                    });
+                });
+
+            },
+
+            incrementAndQueueImage = function () {
+                previousPosition = nextPosition;
+
+                // If nextPosition is equal to the number of images, set to 0
+                if (!firstRun) {
+                    if (nextPosition === options.files.length - 1) {
+                        nextPosition = 0;
+                    } else {
+                        nextPosition += 1;
+                    }
+                }
+
+                queueImage();
+            },
+
+            scanlineInit = function () {
+
+            },
+
+            backgroundInit = function () {
                 // Set properties of container DIV
                 fsbgContainer$.css({
                     position: "fixed",
@@ -60,77 +146,11 @@
                 });
             },
 
-            incrementAndQueueImage = function () {
-                previousPosition = nextPosition;
+            init = function () {
+                backgroundInit();
 
-                // If nextPosition is equal to the number of images, set to 0
-                if (!firstRun) {
-                    if (nextPosition === options.files.length - 1) {
-                        nextPosition = 0;
-                    } else {
-                        nextPosition += 1;
-                    }
-                }
-
-                queueImage();
-            },
-
-            queueImage = function () {
-                // Get the nextPosition image
-                var file = options.files[nextPosition],
-                    imageIdPrefix = "fsbg_image_",
-                    newImgContainerId = imageIdPrefix + nextPosition,
-                    oldImgContainerId = imageIdPrefix + previousPosition
-
-                newImage$ = $("<img />");
-                newImage$.attr("src", file);
-                newImage$.attr("id", newImgContainerId);
-                newImage$.css(
-                    {
-                        "display": "none",
-                        "position": "absolute",
-                        "top": "0px",
-                        "left": "0px"
-                    });
-
-                fsbgContainer$.find("img").addClass("delete-me");
-
-                fsbgContainer$.append(newImage$);
-
-                // We only want to transition in the new image if it's been fully loaded
-                newImage$.one("load", function () {
-                    resizeBackground();
-
-                    // Transition in the new one
-                    newImage$.fadeIn(options.transitionTime, function () {
-                        firstRun == true ? firstRun = false : fsbgContainer$.find(".delete-me").remove();
-                    });
-                });
-
-            },
-
-            resizeBackground = function () {
-                var imageWidth = newImage$.width(),
-                    imageHeight = newImage$.height(),
-                    containerWidth = fsbgContainer$.width(),
-                    containerHeight = fsbgContainer$.height(),
-                    imageRatio = imageWidth / imageHeight,
-                    containerRatio = containerWidth / containerHeight;
-
-                if (imageRatio <= containerRatio) {
-                    newImage$.css({ width: containerWidth + "px", height: "auto" });
-
-                    // offset the top to make it appear centered
-                    if (newImage$.height() > containerHeight) {
-                        newImage$.css({ top: "-" + ((imageHeight - containerHeight) / 2) + "px" });
-                    }
-                } else {
-                    newImage$.css({ height: containerHeight + "px", width: "auto" });
-
-                    // offset the left to make it appear centered
-                    if (newImage$.width() > containerWidth) {
-                        newImage$.css({ left: "-" + ((imageWidth - containerWidth) / 2) + "px" });
-                    }
+                if (options.useScanlines) {
+                    scanlineInit();
                 }
             };
 
